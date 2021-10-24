d5636c460ab3a43c5c39f80e3889931fb59540d3
MesbahAbdullah <mesbahabdullah09@gmail.com>
2021-10-24 18:47:38 +0200

    updating

--- 2-MCAL/4-EXTI/EXTI_private.h
+++ 2-MCAL/4-EXTI/EXTI_private.h
@@ -25,14 +25,20 @@
 #define LINE_14 14
 #define LINE_15 15
 
+/* EXTI Mode */
+#define RISING    0
+#define FALLING   1
+#define ON_CHANGE 2
+
+
 typedef struct
 {
-    u32 EXTI_IMR;
-    u32 EXTI_EMR;
-    u32 EXTI_RSTR;
-    u32 EXTI_FTSR;
-    u32 EXTI_SWIER;
-    u32 EXTI_PR;
+    u32 EXTI_IMR;    /* Interrupt mask register */
+    u32 EXTI_EMR;    /* Event mask register     */
+    u32 EXTI_RTSR;   /* Rising trigger selection register  */
+    u32 EXTI_FTSR;   /* Falling trigger selection register */
+    u32 EXTI_SWIER;  /* Software interrupt event register  */
+    u32 EXTI_PR;     /* Pending register */
 } EXTI_t;
 
 #define EXTI ((volatile EXTI_t *)0x40010400)
--- 2-MCAL/4-EXTI/EXTI_program.c
+++ 2-MCAL/4-EXTI/EXTI_program.c
@@ -14,9 +14,49 @@
 
 void MEXTI_voidInit()
 {
-    #if 
+#if EXTI_MODE == RISING
+    SET_BIT(EXTI->EXTI_RTSR, EXTI_LINE);
+#elif EXTI_MODE == FAILING
+    SET_BIT(EXTI->EXTI_FTSR, EXTI_LINE);
+#elif EXTI_MODE == ON_CHANGE
+    SET_BIT(EXTI->EXTI_RTSR, EXTI_LINE);
+    SET_BIT(EXTI->EXTI_FTSR, EXTI_LINE);
+#else
+    /* ERROR */
+#endif
+    /* Disable interrupt */
 }
-void MEXTI_voidEnableEXTI ( u8 Copy_u8Line);
-void MEXTI_voidDisableEXTI( u8 Copy_u8Line);
-void MEXTI_voidSwTrigger  ( u8 Copy_u8Line);
+void MEXTI_voidEnableEXTI(u8 Copy_u8Line)
+{
+    SET_BIT(EXTI->EXTI_IMR, Copy_u8Line);
+}
+void MEXTI_voidDisableEXTI(u8 Copy_u8Line)
+{
+    CLR_BIT(EXTI->EXTI_IMR, Copy_u8Line);
+}
+/* SW interrupt */
+void MEXTI_voidSwTrigger(u8 Copy_u8Line)
+{
+    SET_BIT(EXTI->EXTI_SWIER, Copy_u8Line);
+}
 
+/* changing in run time */
+void MEXTI_voidSetSignalLatch(u8 Copy_u8Line, u8 Copy_u8Mode)
+{
+    switch (Copy_u8Mode)
+    {
+    case RISING:
+        SET_BIT(EXTI->EXTI_RTSR, EXTI_LINE);
+        break;
+    case FALLING:
+        SET_BIT(EXTI->EXTI_FTSR, EXTI_LINE);
+        break;
+    case ON_CHANGE:
+        SET_BIT(EXTI->EXTI_RTSR, EXTI_LINE);
+        SET_BIT(EXTI->EXTI_FTSR, EXTI_LINE);
+        break;
+    default:
+        /* Error */
+        break;
+    }
+}
